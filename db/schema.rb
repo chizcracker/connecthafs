3195f001690bfdd4249001400e88545aa3fdd26e
Jeung Won Kim <jeungwok@gmail.com>
2016-04-24 14:53:06 -0700

    we can't use 'class' for db attr

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424182446) do
+ActiveRecord::Schema.define(version: 20160424215030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20160424182446) do
   create_table "users", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.string   "name"
     t.string   "phonenumber"
-    t.integer  "class"
+    t.integer  "class_year"
     t.string   "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
