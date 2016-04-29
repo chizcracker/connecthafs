7d7b49d82644b2958743e2872bb98667d75c9238
Jeung Won Kim <jeukim@groupon.com>
2016-04-28 22:01:41 -0700

    missing test

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe GroupsController, type: :controller do
-
+describe GroupsController, type: :controller do
   describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    context "with existing group" do
+      let(:group_id) { Group.create!.id }
+
+      it "returns http success" do
+        get :show, id: group_id
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "with non-existing group" do
+      let(:group_id) { SecureRandom.uuid }
+
+      it "returns 404" do
+        expect{ get :show, id: group_id }.to raise_exception(ActiveRecord::RecordNotFound)
+      end
     end
   end
-
 end
