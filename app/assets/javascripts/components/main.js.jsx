8dc38cd8d26d1bb1e1b85656203eea7f9f83d041
Lucas Cheon <inwoo@inwoo-mbp.local>
2016-05-03 22:50:08 -0700

    Main have user rows

--- app/assets/javascripts/components/main.js.jsx
+++ app/assets/javascripts/components/main.js.jsx
@@ -1,6 +1,20 @@
 var Main = React.createClass({
+  propTypes: {
+    results: React.PropTypes.array
+  },
 
   render: function() {
+    var userRows = this.props.results.map(function(user) {
+      return (
+        <UserRow
+          // Assigning a key to avoid
+          // https://fb.me/react-warning-keys
+          key={user.id}
+          user={user}
+        />
+      );
+    });
+
     return (
       <div>
         <button>
@@ -15,7 +29,26 @@ var Main = React.createClass({
             Search
           </button>
         </div>
+        {userRows}
       </div> 
+    );
+  }
+});
+
+var UserRow = React.createClass({
+  propTypes: {
+    user: React.PropTypes.object
+  },
+
+  render: function() {
+    var user = this.props.user;
+    return (
+      <div style={{ border: '2px solid green', marginTop: '10px', width: '200px' }}>
+        <a href={'/users/' + user.id}>{user.name}</a><br />
+        {user.address}<br />
+        {user.phone_number}<br />
+        {user.class_year}<br />
+      </div>
     );
   }
 });
