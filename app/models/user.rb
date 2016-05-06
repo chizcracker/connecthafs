bec0a6ec39aadb16ef69c9f920ca7e9c4b064e5d
Jeung Won Kim <jeukim@groupon.com>
2016-05-06 15:08:48 -0700

    devise setup (migration)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
-    has_many :user_works
-    has_many :works, through: :user_works
-    has_many :user_educations
-    has_many :educations, through: :user_educations
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
+
+  has_many :user_works
+  has_many :works, through: :user_works
+  has_many :user_educations
+  has_many :educations, through: :user_educations
 end
